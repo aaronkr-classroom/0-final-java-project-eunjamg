65ab5fbd842a47c37d7dde12f1b5502a08339975
dnjs275 <162276867+eunjamg@users.noreply.github.com>
2024-05-07 16:08:30 +0900

    프로젝트 finshmarket 전은장

--- src/CartInterface.java
+++
@@ -1,7 +0,0 @@
-public interface CartInterface {
-     void printBookList(Book[] p);
-     boolean isCartInBook(String id); // 책의 함수 이름은 isCartInBook()
-     void insertBook(Book p);
-     void removeCart(int numId);
-     void deleteBook();
-}
